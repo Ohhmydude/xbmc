fe583db099068721f500b6617f7b3b7a524aab11
sarbes <sarbes@kodi.tv>
2020-05-13 03:51:10 +0200

    simplifiction of gui shaders

---
+++ system/shaders/GL/1.5/gl_guishader_frag.glsl
@@ -0,0 +1,35 @@
+#version 150
+
+uniform sampler2D m_samp0;
+uniform sampler2D m_samp1;
+uniform vec4 m_unicol;
+in vec4 m_colour;
+in vec4 m_cord0;
+in vec4 m_cord1;
+out vec4 fragColor;
+
+
+void main ()
+{
+#if defined(SM_TEXTURE_NOBLEND)
+  fragColor = texture(m_samp0, m_cord0.xy);
+#elif defined(SM_TEXTURE) || defined(SM_TEXTURE_LIM)
+  fragColor = texture(m_samp0, m_cord0.xy) * m_unicol;
+#elif defined(SM_MULTI)
+  fragColor = texture(m_samp0, m_cord0.xy) * texture(m_samp1, m_cord1.xy);
+#elif defined(SM_MULTI_BLENDCOLOR)
+  fragColor = texture(m_samp0, m_cord0.xy) * texture(m_samp1, m_cord1.xy) * m_unicol;
+#elif defined(SM_FONTS)
+  fragColor.rgb = m_colour.rgb;
+  fragColor.a = m_colour.a * texture(m_samp0, m_cord0.xy).r;
+#else //SM_DEFAULT
+  fragColor = m_unicol;
+#endif
+
+#if defined(KODI_LIMITED_RANGE) && !defined(SM_TEXTURE_LIM)
+  fragColor.rgb *= (235.0-16.0) / 255.0;
+  fragColor.rgb += 16.0 / 255.0;
+#elif !defined(KODI_LIMITED_RANGE) && defined(SM_TEXTURE_LIM)
+  fragColor.rgb = clamp((fragColor.rgb-(16.0/255.0)) * 255.0/219.0, 0, 1);
+#endif
+}
